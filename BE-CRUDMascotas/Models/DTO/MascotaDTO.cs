11c5ee3055980aad2655709e3bb9d1cf3aff0570
Javier Delgado <JADelgado@proveedor.teco.com.ar>
2023-01-27 17:09:19 -0300

    Add project files.

---
+++ BE-CRUDMascotas/Models/DTO/MascotaDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace BE_CRUDMascotas.Models.DTO
+{
+    public class MascotaDTO
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+        public string Raza { get; set; } = string.Empty;
+
+        public string Color { get; set; } = string.Empty;
+
+        public int Edad { get; set; }
+
+        public float Peso { get; set; }
+    }
+}
---
+++ BE-CRUDMascotas/Models/Mascota.cs
@@ -0,0 +1,18 @@
+﻿namespace BE_CRUDMascotas.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+        public string Raza { get; set; } = string.Empty;
+
+        public string Color { get; set; } = string.Empty;
+
+        public int Edad { get; set; }
+
+        public float Peso { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
